115c15bf28bd86a07994a494fae0d6035115e3ae
Sisop <agustiniglesias99@gmail.com>
2022-04-09 15:11:02 -0300

    agrego encabezado y funcion de ayuda

--- Ejercicio2.sh
+++ Ejercicio2.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
-if [ $1 -gt 3 ]; then
-    exit 0
-else
-    exit 1
+####################   ENCABEZADO    ##########################
+#							      							  #
+#	Nombre del script: Ejercicio2.sh		     			  #
+#	Número de APL : 1				     					  #
+#	Número de Ejercicio: 2                                    #
+#   Primer Entrega      				 				      #
+#							    							  #
+#  INTEGRANTES:						  					      #
+#	Morandi Mayra          41454827 				          #
+#	Iglesias Agustin Ariel 41894233							  #
+#	Zarzycki Hernan        39244031         				  #
+#	Monteros Matias Javier 40886497							  #
+#	Casaux Nicolas         39347293 						  #
+#							      							  #
+##############################################################
+
+
+function ayuda() {
+    echo "El proposito de este script es verificar que ciertas cadenas no se encuentren en los archivos dentro un directorio específico"
+    echo "Para ejecutar el script ingrese:";
+    echo "./Ejercicio2.sh --diff miDirectorioDiff --check misCadenas.txt"
+    echo "El parámetro --diff indica el directorio que contiene los archivos a analizar"
+    echo "El parámetro --check indica el archivo de texto, que contiene las cadenas que van a tenerse en cuenta para analizar los archivos"
+}
+
+if [ $# -eq 0 ]; then
+    echo "No se ingresaron parámetros. Para obtener informacion sobre uso del script: -h -help o -?"
+    exit
+elif [ "$1" = "-help" ] || [ "$1" = "-h" ] || [ "$1" = "-?" ]; then
+        ayuda
+        exit
 fi
+
+for i in "$@"
+do
+ if [ "$i" == "--diff" ]; then
+    directorio="$2";
+ fi
+ if [ "$i" == "--check" ]; then
+    archCadenas="$2";
+ fi
+ shift
+done
+
+echo $directorio
+echo $archCadenas
---
+++ dev.md
@@ -0,0 +1,1 @@
+#archivo prueba dev

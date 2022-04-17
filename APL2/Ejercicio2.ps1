7de3142de77caf0f709c77d9e703ca89e639e373
agustiniglesias99 <agustiniglesias99@gmail.com>
2022-04-17 12:47:05 -0300

    se agregan validaciones por si el archivo de cadenas o el directorio estan vacios

--- APL2/Ejercicio2.ps1
+++ APL2/Ejercicio2.ps1
@@ -1,3 +1,20 @@
+
+####################   ENCABEZADO    ##########################
+#							      							  #
+#	Nombre del script: Ejercicio2.sh		     			  #
+#	Número de APL : 1				     					  #
+#	Número de Ejercicio: 2                                    #
+#   Primer Entrega      				 				      #
+#							    							  #
+#  INTEGRANTES:						  					      #
+#	Morandi Mayra               41454827 				      #
+#	Iglesias Agustin Ariel      41894233					  #
+#	Zarzycki Hernan             39244031         			  #
+#	Monteros Matias Javier      40886497					  #
+#	Casaux Nicolas              39347293 					  #
+#							      							  #
+##############################################################
+
 <#
 .SYNOPSIS
      "El proposito de este script es verificar que ciertas cadenas no se encuentren en los archivos dentro un directorio específico"

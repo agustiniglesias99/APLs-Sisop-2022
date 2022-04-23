ff015b02d47e57c18a11f1d5be3930018a4a1c9e
Agustin Iglesias <agustiniglesias99@gmail.com>
2022-04-23 16:06:01 -0300

    se crean todos los archivos de prueba pero sin informacion

--- APL2/Ejercicio2/Ejercicio2.ps1
+++ APL2/Ejercicio2/Ejercicio2.ps1
@@ -1,8 +1,8 @@
 
-####################   ENCABEZADO    ##########################
+<#
 #							      							  #
 #	Nombre del script: Ejercicio2.sh		     			  #
-#	Número de APL : 1				     					  #
+#	Número de APL : 2   			     					  #
 #	Número de Ejercicio: 2                                    #
 #   Primer Entrega      				 				      #
 #							    							  #
@@ -13,33 +13,34 @@
 #	Monteros Matias Javier      40886497					  #
 #	Casaux Nicolas              39347293 					  #
 #							      							  #
-##############################################################
+#>
 
 <#
 .SYNOPSIS
      "El proposito de este script es verificar que ciertas cadenas no se encuentren en los archivos dentro un directorio específico"
      "Para ejecutar el script ingrese:";
 .DESCRIPTION  
-     "El parámetro --diff indica el directorio que contiene los archivos a analizar"
-     "El parámetro --check indica el archivo de texto, que contiene las cadenas que van a tenerse en cuenta para analizar los archivos"
+     "El parámetro -diff indica el directorio que contiene los archivos a analizar"
+     "El parámetro -check indica el archivo de texto, que contiene las cadenas que van a tenerse en cuenta para analizar los archivos"
 .EXAMPLE
-    "./Ejercicio2.sh --diff miDirectorioDiff --check misCadenas.txt"
+    "./Ejercicio2.sh -diff miDirectorioDiff -check misCadenas.txt"
 #>
 
 Param (
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty]
     [string]
-    $Directorio,
+    $diff,
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty]
     [string]
-    $archCadenas
+    $check
 )
 
 $existe = Test-Path -Path $Directorio
 
 if ($existe -ne $true) {
     Write-Host "El direcotrio $Directorio no existe"
-    exit 1
+    exit 2
 }
+

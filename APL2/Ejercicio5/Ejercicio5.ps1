cea1acda3a98855216e65ea7603651e284c5baf7
Agustin Iglesias <agustiniglesias99@gmail.com>
2022-04-28 19:56:29 -0300

    se logro sumar todos los valores para cada producto por separado incluso entre archivos

--- APL2/Ejercicio5/Ejercicio5.ps1
+++ APL2/Ejercicio5/Ejercicio5.ps1
@@ -37,33 +37,98 @@ Param (
     $backup
 )
 
-#region Declaracion de funciones
+ #region Declaracion de funciones
 
-function obtener_producto_mas_vendido{
+function obtener_producto_mas_vendido_dia {
+    param (
+        [string]$archivoDia
+    )
+        $max=0;
+        $contenido=(Get-Content $archivoDia)
+        $registros=$contenido -split "\n"
+        for($i=1;$i -lt $registros.Length;$i++){
+            $current=($registros[$i] -split "\|")[1]
+            if($current -gt $max){
+                $max=$current;
+            }
+        }
+    return $max
+}
+
+function obtener_producto_mas_vendido_sucursal{
     param (
         [string]$sucursal
     )
     $listadoDias = New-Object System.Collections.ArrayList
-    $listadoDias = @(Get-ChildItem -Path $sucursal -File | Where-Object {$_.Name -match '*.txt$'})
+    $listadoDias = @(Get-ChildItem -Path $sucursal -File)# | Where-Object {$_.Name -match '*.txt$'})
+    $max=0;
+    foreach($dia in $listadoDias){
+        $current=(obtener_producto_mas_vendido_dia $dia)
+        if($current -gt $max){
+            $max=$current;
+        }
+    }
+    return $max
 }
 
+function obtener_producto_mas_vendido_sucursal2{
+    param (
+        [string]$sucursal
+    )
+    $listadoDias = New-Object System.Collections.ArrayList
+    $productosHash = New-Object System.Collections.Hashtable
+    $listadoDias = @(Get-ChildItem -Path $sucursal -File) #obtengo todos los dias de una sucursal
+    $keyMax;
+    $valorMax=0;
+    $valorActual=0;
+    
+    foreach($dia in $listadoDias){
+        
+        $contenido=(Get-Content $dia)   #cargo en un array todo el contenido del archivo dia
+        $registros=$contenido -split "\n"   #spliteo el array anterio linea por linea
+        if($registros[1].Length -gt 0){
+            for($i=1;$i -lt $registros.Length;$i++){    #inicio i en 1 para saltear el texto del formato
 
-#endregion
+                $productoActual = ($registros[$i] -split "\|")[0];
+                $valorActual = [int]($registros[$i] -split "\|")[1];
 
-if ((Test-Path -Path $entrada) -ne $true) {
-    Write-Host "El direcotrio $entrada no existe"
-    exit 1
+                if( $productosHash.ContainsKey($productoActual)){
+                    $productosHash[$productoActual] += $valorActual;
+                }
+                else{
+                    $productosHash.Add("$productoActual", $valorActual);
+                }
+            }
+        }
+    }
+
+    foreach($item in $productosHash.GetEnumerator()){
+        if($valorMax -lt $item.value){
+            $valorMax=$item.Value;
+            $keyMax=$item.Key;
+        }
+    }
+    Write-Host "el producto mas vendido para la sucursal" (Get-ChildItem $sucursal)[0].directory.name "fue " $keyMax "con un total de " $productosHash[$keyMax] "unidades"
 }
 
-if (($salida.ToString().Length -gt 0) -and (Test-Path -Path $salida) -ne $true){
-    Write-Host "El directorio $salida no existe"
-    exit 1
-}
+# #endregion
 
-$listaSucursales = New-Object System.Collections.ArrayList
+obtener_producto_mas_vendido_sucursal2 "C:\Users\agust\repos\APLs-Sisop-2022\APL2\Ejercicio5\Entrada\Sucursal1"
 
-$listaSucursales = @(Get-ChildItem -Path $entrada -Recurse -Directory)
+# if ((Test-Path -Path $entrada) -ne $true) {
+#     Write-Host "El direcotrio $entrada no existe"
+#     exit 1
+# }
 
-foreach($directorio in $listaSucursales){
+# if (($salida.ToString().Length -gt 0) -and (Test-Path -Path $salida) -ne $true){
+#     Write-Host "El directorio $salida no existe"
+#     exit 1
+# }
 
-}
+# $listaSucursales = New-Object System.Collections.ArrayList
+
+# $listaSucursales = @(Get-ChildItem -Path $entrada -Recurse -Directory)
+
+# foreach($directorio in $listaSucursales){
+
+# }
